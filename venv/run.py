8497f7aad98f96324715661ba5bef13e7d552a42
gokul_krishna <gokulkrishnagk62@gmail.com>
2023-12-11 01:49:32 -0500

    changed run file

--- venv/run.py
+++ venv/run.py
@@ -4,6 +4,6 @@ import config
 # Only run the app if this script is being executed directly (not imported as a module)
 if __name__ == '__main__':
     # Start the app and listen on all available network interfaces on the configured port
-    # app.run(host='0.0.0.0', port=8888)
-    app.run(host='0.0.0.0', port=config.SERVER_PORT)
+    app.run(host='0.0.0.0', port=8888)
+    # app.run(host='0.0.0.0', port=config.SERVER_PORT)
     print("Server is listening... ")
